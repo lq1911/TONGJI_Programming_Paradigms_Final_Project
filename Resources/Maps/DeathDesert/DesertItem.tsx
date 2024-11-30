46a7113c79922519472dde70df548c757e334426
Wing_WR <wingwr998@gmail.com>
2024-12-01 03:36:56 +0800

    20241201:添加了沙漠地图资源

---
+++ Resources/Maps/DeathDesert/DesertItem.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="DesertItem" tilewidth="32" tileheight="32" tilecount="1984" columns="62">
+ <image source="BROWNROCK_full_tileset_v2.png" width="1984" height="1024"/>
+</tileset>
