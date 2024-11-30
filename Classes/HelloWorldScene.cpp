0ee7b8e6563a1d22aa6c984b44b8fe088f61966d
Wing_WR <wingwr998@gmail.com>
2024-11-30 22:44:33 +0800

    20241130修改回了主分支

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -93,10 +93,7 @@ bool HelloWorld::init()
         // add the sprite as a child to this layer
         this->addChild(sprite, 0);
     }
-
-   
-
-    
+ 
     // 开始游戏按钮
     auto button = cocos2d::ui::Button::create("Button/PlayButton.png", "Button/PlayButtonClicked.png", "Button/PlayButtonClicked.png");
     button->ignoreContentAdaptWithSize(false);  // 启用内容大小适配
@@ -118,7 +115,7 @@ bool HelloWorld::init()
     // 按钮点击事件监听器
     button->addTouchEventListener([](Ref* sender, cocos2d::ui::Widget::TouchEventType type) {
         if (type == ui::Widget::TouchEventType::ENDED) {
-            auto scene = SetPlayerScene::createScene();          
+            auto scene = SetPlayerScene::createScene();
             Director::getInstance()->replaceScene(TransitionFade::create(1.0f, scene, Color3B::BLACK)); // 场景切换
             CCLOG("Button clicked, switching to SetPlayerScene.");
         }
