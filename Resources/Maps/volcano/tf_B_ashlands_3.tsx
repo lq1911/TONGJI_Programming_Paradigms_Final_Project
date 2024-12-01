ce97374f8784e87e2604229ee98cda376a07524a
Drinkin天天听 <977250079@qq.com>
2024-12-01 15:26:04 +0800

    添加了volcano地图

---
+++ Resources/Maps/volcano/tf_B_ashlands_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tf_B_ashlands_3" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="../../Tiles Material/tf_ashlands/3x_RMMV/tf_B_ashlands_3.png" width="768" height="768"/>
+</tileset>
