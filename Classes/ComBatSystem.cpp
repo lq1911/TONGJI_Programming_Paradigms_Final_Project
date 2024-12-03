90f49d3276d2cc73cc843a404df75c57c7bae9b1
ZHOU DA <zd205428@outlook.com>
2024-12-03 20:53:09 +0800

    12/3增加了战斗系统

--- Classes/ComBatSystem.cpp
+++ Classes/ComBatSystem.cpp
@@ -1,4 +1,66 @@
-#include "BattleSystem.h"
+#include "CombatSystem.h"
 USING_NS_CC;
 
 
+// 基本动作
+auto attackAction = MoveBy::create(1.0f, Vec2(100, 0));
+
+
+//攻击
+void Creature::Attack(Creature target,int idx) {
+	//计算攻击范围
+	//如果target在攻击范围内
+	//target.Hurt(atk);
+	//动画实现
+	// 图片名前缀:除编号部分
+	string s;
+	if (idx == 1 || idx == 2)
+		s = "Role/Player/" + name + "/attack" + std::to_string(idx) + "/" + name + "_atk" + std::to_string(idx) + "_";
+	else if (idx == 3)
+		s = "Role/Player/" + name + "/attack_final/" + name + "_final_";
+	// 帧数
+	int count = 0;
+	if (name == "Arthur") {
+		if (idx == 1)
+			count = 10;
+		else if (idx == 2)
+			count = 12;
+		else if (idx == 3)
+			count = 15;
+	}
+	else if (name == "Longbow") {
+		if (idx == 1)
+			count = 7;
+		else if (idx == 2)
+			count = 12;
+		else if (idx == 3)
+			count = 18;
+	}
+	// 帧动画准备
+	Vector<SpriteFrame*> animFrames;//储存动画帧的容器
+	animFrames.reserve(count);//分配内存
+	//创建（单个）动画对象，将其装入容器中
+	for (int i = 1; i <= count; i++) {
+		auto texture = Director::getInstance()->getTextureCache()->addImage(s + std::to_string(i) + ".png");
+		float width = texture->getPixelsWide();
+		float height = texture->getPixelsHigh();
+		Rect rectInPixels(0, 0, width, height);
+		auto spriteFrame = SpriteFrame::createWithTexture(
+			texture,
+			CC_RECT_PIXELS_TO_POINTS(rectInPixels)
+		);
+		animFrames.pushBack(spriteFrame);
+	}
+	//动画
+	Animation* animation = Animation::createWithSpriteFrames(animFrames, 0.2f);
+	//Animate动作
+	Animate* animate = Animate::create(animation);
+	// mySprite->runAction(RepeatForever::create(animate));
+	mySprite->runAction(animate);  // 播放一次
+	scene->addChild(mySprite);//添加至场景
+}
+//静止
+void Creature::Show() {
+	mySprite->setPosition(Vec2(x, y));
+	scene->addChild(mySprite);
+}
--- Classes/ComBatSystem.h
+++ Classes/ComBatSystem.h
@@ -1,28 +1,37 @@
-#ifndef __Battle_System_H__
-#define __Battle_System_H__
+#ifndef __Combat_System_H__
+#define __Combat_System_H__
 #include "cocos2d.h"
 USING_NS_CC;
 using namespace std;
-// 基本动作
-auto attackAction =  MoveBy::create(1.0f,  Vec2(100, 0));
-
-
-
+struct Equipment {//交由背包完成
+	int a;
+};
+struct Bonus {
+	int exp;
+	int object;
+	Equipment equipment;
+};
 class Creature {
 private:
-	string name;//就是who
-	int hp;
-	int mp;
-	int atk;
-	int def;
-	int speed;
-	int level;
-	
+	string name;//就是name
+	int hp;//基础生命
+	int mp;//基础魔力
+	int atk;//基础攻击值
+	int atk_range;//攻击范围
+	int def;//基础防御值
+	int speed;//速度
+	int level;//等级
+	Scene* scene;//所在场景？
+	int x;//坐标
+	int y;
+	Sprite* mySprite;//精灵
+
 public:
-	Creature(string name,int hp, int mp, int atk, int def, int speed, int level) :
-		name(name),hp(hp), mp(mp), atk(atk), def(def), speed(speed), level(level) {
-		;
+	Creature(string name,int hp, int mp, int atk, int def, int speed, int level,int x,int y) :
+		name(name),hp(hp), mp(mp), atk(atk), def(def), speed(speed), level(level) {//缺少对scene的初始化
+		Level_Bonus();
 	}
+	// 空的构造函数，供调试
 	Creature() {
 		hp = 1;
 		mp = 1;
@@ -31,42 +40,71 @@ public:
 		speed = 10;
 		level = 1;
 	}
-	virtual void Attack();
-	virtual void Combo();
+	// 攻击,对s赋值时最好不要使用Player和Monster，或将Monster的图片也装进role/player文件夹里
+	//s = "Role/     ->Player<-     /" + name + "/attack" + std::to_string(idx) + "/" + name + "_atk" + std::to_string(idx) + "_";
+	//以下几个函数相同
+	virtual void Attack(Creature target,int idx);
+	//受伤
+	void Hurt(int atk);
+	//死亡
+	void Die();
+	//恢复
+	virtual void Heal();
+
+	// 静止
+	virtual void Show();
+	//移动
+	virtual void Move();
+	//等级加成
+	virtual void Level_Bonus();
+	// 转变场景
+	virtual void ChangeScene(Scene* sc)  {//需要修改，与地图对接，需要地图类返回GetScene的值（一个类型为Scene*的scene)
+		scene = sc;
+	}
 };
-void Creature::Attack() {
-	int a;
-}
+//角色类
 class Player :public Creature {
 private:
-	
+	int current_exp;//角色现有经验值
+	int next_level_exp;//达到下一级所需经验值
 public:
-	Player(string name,int hp, int mp, int atk, int def, int speed, int level) :
-		Creature(name,hp, mp, atk, def, speed, level) {
-		
+	Player(string name, int hp, int mp, int atk, int def, int speed, int level, int x, int y) :
+		Creature(name, hp, mp, atk, def, speed, level, x, y) {
+		string s = "Role/Player/" + name + "/" + name;
+		auto mySprite = Sprite::create(s + ".png");
+		mySprite->setPosition(Vec2(x, y));
+		Level_Bonus();
 	}
-	void Attack() override {
+	//装备，加成数值，不考虑贴图,需要与背包对接,参数是一个Equipment结构体，结构体内需要含atk和def，即攻击和防御
+	void Equipment() {
 		;
 	}
-
+	// 技能，以组合技形式出现
+	 void Combo();
+	 //获得奖励，参数Bonus结构体,结构体内需含有经验值，物品部分交给背包
+	 void GetBonus() {
+		 ;
+	 }
+	 //战斗状态
+	 void IsInCombat();
 };
-
+//怪物类
 class Monster :public Creature {
 private:
+	Bonus bonus;
+	int base_exp;//怪物的基础经验值
 public:
-	
+	Monster(string name, int hp, int mp, int atk, int def, int speed, int level, int x, int y) :
+		Creature(name, hp, mp, atk, def, speed, level, x, y) {
+		Level_Bonus();
+		;//暂待
+	}
+	//奖励
+	void Bonus();
+	//触发战斗
+	void StartFight();
+	//寻路，徘徊，或静止
+
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-#endif// __Battle_System_H__
+#endif// __Combat_System_H__
