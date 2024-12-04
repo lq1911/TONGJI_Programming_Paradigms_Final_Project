887434c5cf7e0d0beedb10c9d8a38c510340da1a
cbx6666 <2353120@tongji.edu.cn>
2024-12-04 20:21:02 +0800

    实现了物品类的继承和多态，实现了物品的添加、丢弃、消耗"
    "
    ‘
    ’

--- Classes/Item.h
+++ Classes/Item.h
@@ -13,17 +13,54 @@ using namespace std;
 class item
 {
 public:
+    // 物品的名称
 	string name;
+    // 物品的图像
 	cocos2d::Sprite* image;
-    item(const string& itemName, const string& imagePath)
-        : name(itemName)
+    // 物品的描述
+    string description;
+
+    // 构造函数，需要给物品的三种成员变量赋值
+    item(const string& itemName, const string& imagePath, const string& itemDescription)
+        : name(itemName), description(itemDescription)
     {
-        image = cocos2d::Sprite::create(imagePath); // 鍒涘缓鐗╁搧鍥剧墖
+        image = cocos2d::Sprite::create(imagePath); // 创建物品图片
     }
+
+    // 获取物品的名称
     string getName() const
     {
         return name;
     }
+
+    // 获取物品的属性
+    string getDescription() const
+    {
+        return description;
+    }
+
+    // 物品发挥功能
+    void itemFunction()
+    {
+
+    }
+    // 添加虚析构函数，使得类变为多态类
+    virtual ~item() {}
+};
+// 
+class equipment:public item
+{
+public:
+    equipment(const string& itemName, const string& imagePath, const string& itemDescription)
+        : item(itemName, imagePath, itemDescription) {}
+    void equip();
 };
 
+class consumable : public item
+{
+public:
+    consumable(const string& itemName, const string& imagePath, const string& itemDescription)
+        : item(itemName, imagePath, itemDescription) {}
+    void consume();
+};
 #endif
