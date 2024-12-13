6ae49b9c18f0a16f58a3e1e3c30ce14fdb81492a
Wing_WR <wingwr998@gmail.com>
2024-12-13 22:11:18 +0800

    20241213:添加了一张室内地图

---
+++ Resources/Maps/Indoors/Library.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Library" tilewidth="32" tileheight="32" tilecount="736" columns="46">
+ <image source="libassetpack-tiled.png" width="1488" height="528"/>
+</tileset>
