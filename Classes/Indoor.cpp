5610a9c9d39a9931a77a4e0098999a5c079157ca
LQ <qi.linnnnn2004@gmail.com>
2024-12-18 15:41:49 +0800

    20241218:修改NPC的小bug

---
+++ Classes/Indoor.cpp
@@ -0,0 +1,43 @@
+//#include "Indoor.h"
+//
+//Indoor::Indoor(Player* origin_player) {
+//	player = origin_player;
+//}
+//
+//Scene* Indoor::createScene() {
+//	return Indoor::create();
+//}
+//
+//bool Indoor::init() {
+//	if (!Scene::init()) {
+//		return false;
+//	}
+//
+//	//获取屏幕尺寸
+//	visibleSize = Director::getInstance()->getVisibleSize();
+//
+//	//初始化室内地图
+//	this->LoadMap();
+//
+//	//初始化玩家位置
+//	player->setPosition(0, 0);
+//
+//
+//	//设置相机跟随玩家
+//	this->MainCameraFollowPlayer();
+//
+//	return true;
+//}
+//
+//void Indoor::LoadMap() {
+//	auto IndoorMap = TMXTiledMap::create("Maps/Indoors/InDoors.tmx");
+//	IndoorMap->setScale(1.0f);
+//	IndoorMap->setAnchorPoint(Vec2(0.5f, 0.5f));
+//	InitialObstacle(IndoorMap);
+//
+//	this->addChild(IndoorMap);
+//}
+//
+//void Indoor::InitialCamera() {
+//	camera = getDefaultCamera();    //获取默认相机
+//}
---
+++ Classes/Indoor.h
@@ -0,0 +1,32 @@
+//#ifndef __IN_DOOR_H__
+//#define __IN_DOOR_H__
+//
+//#include "SetMap.h"
+//#include "cocos2d.h"
+//USING_NS_CC;
+//
+//class Indoor :public SetMap {
+//private:
+//	Size visibleSize;    //屏幕大小
+//
+//	Monster* monster_1;     //怪物1
+// 	Monster* monster_2;    //怪物2
+//	Monster* monster_3;    ///怪物3
+//
+//	NPC* npc_1;    //npc1
+//	NPC* npc_2;    //npc2
+//	NPC* npc_3;    //npc3
+//
+//	Player* player;    //玩家
+//
+//	Camera* camera;    //摄像机
+//public:
+//	Indoor(Player* origin_player);
+//	virtual bool init();
+//	static cocos2d::Scene* createScene();
+//	void LoadMap() override;
+//	void InitialCamera() override;
+//
+//};
+//
+//#endif // __IN_DOOR_H__
